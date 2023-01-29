7a048f8f4e0dabf4a0c5b2dab3784e00236af653
Moh Rizky Dwi Jayanto <109938825+R1nto@users.noreply.github.com>
2023-01-29 17:58:25 +0700

    Add files via upload

---
+++ vid 3/Kontak.js
@@ -0,0 +1,11 @@
+function Kontak() {
+    return (
+      <div className="App">
+        <h1>Kontak Rinto</h1>
+        <p>Sidoarjo</p>
+        <img src="images/logo.png" alt="" srcset=""/>
+      </div>
+    );
+  }
+  
+  export default Kontak;
