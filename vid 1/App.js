4e660a96094a695a58d5d8c13cfd65287ac370ad
Moh Rizky Dwi Jayanto <109938825+R1nto@users.noreply.github.com>
2023-01-29 17:02:06 +0700

    vid 1
    Tutorial Belajar React JS #1 Instalasi React JS

---
+++ vid 1/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Belajar React Js</h1>
+      <p>Belajar React Js</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ vid 1/pages/Kontak.js
@@ -0,0 +1,10 @@
+function Kontak() {
+    return (
+      <div className="App">
+        <h1>Kontak Rinto</h1>
+        <p>Sidoarjo</p>
+      </div>
+    );
+  }
+  
+  export default Kontak;
