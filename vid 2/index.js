91c5e1a1618825fb68fb975bce3c6180001878ff
Moh Rizky Dwi Jayanto <109938825+R1nto@users.noreply.github.com>
2023-01-29 17:35:24 +0700

    Add files via upload

---
+++ vid 2/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Kontak from './pages/Kontak';
+import Nav from './pages/Nav';
+import Sejarah from './pages/Sejarah';
+import Tentang from './pages/Tentang';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Nav />
+    <App />
+    <Kontak />
+    <Sejarah />
+    <Tentang />
+  </React.StrictMode>
+);
+
+reportWebVitals();
---
+++ vid 2/pages/Sejarah.js
@@ -0,0 +1,10 @@
+function Sejarah() {
+    return (
+      <div className="App">
+        <h1>Sejarah Rinto</h1>
+        <p>Isi Sejarah Rinto</p>
+      </div>
+    );
+  }
+  
+  export default Sejarah;
